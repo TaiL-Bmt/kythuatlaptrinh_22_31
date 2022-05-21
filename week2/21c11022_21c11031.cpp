47e847bd900ebbd3118dea3b28c83e20901a2abc
TaiL <trongtaile1990@gmail.com>
2022-05-21 14:39:09 +0700

    Update bai 4,5,6 week2

--- week2/21c11022_21c11031.cpp
+++ week2/21c11022_21c11031.cpp
@@ -1,10 +1,64 @@
+#include <cmath>
 #include <iostream>
 
 // prototype
+void sum3();
+void sum4();
+void dem_so_ki_so();
 
 int main()
 {
-    std::cout << "hello world\n";
+    //sum3() Calculate S(x,n)=x + x^2 + x^3 + ... + x^n
+    //sum3();
+
+    //sum4() Calculate S(n)= sqrt(2 + sqrt(2 + ....
+    //sum4();
+
+    // Dem so luong ki so cua so nguyen duong
+    dem_so_ki_so();
+
     return 0;
 }
 
+void sum3()
+{
+    int n = 0;
+    float x = 0.0;
+    float result = 0;
+    float temp = 1;
+    std::cout << "Calculate S(x,n) = x + x^2 + x^3 + ... + x^n\n";
+    std::cout << "Enter n: "; std::cin >> n;
+    std::cout << "x: "; std::cin >> x;
+
+    for (int i = 1; i <= n; i++) {
+        temp *= x;
+        result += temp;
+    }
+
+    std::cout << "Result is " << result << std::endl;
+}
+
+void sum4()
+{
+    int n = 0;
+    float result = 0;
+    std::cout << "Calculate S(n)= sqrt(2 + sqrt(2 + .... n times\n";
+    std::cout << "Enter n: "; std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        result = sqrt(2 + sqrt(result));
+    }
+    std::cout << "Result is : " << result << std::endl;
+}
+
+void dem_so_ki_so()
+{
+    int n = 0;
+    float result = 0;
+    std::cout << "Dem so ki so cua so nguyen duong n\n";
+    std::cout << "Enter n: "; std::cin >> n;
+    while (n > 0) {
+        result++;
+        n /= 10;
+    }
+    std::cout << "So ki so cua " << n << " la: " << result << std::endl;
+}
